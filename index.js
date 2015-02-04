727c18b498fa7416f047c4ef1e28b8ad324ea69b
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-03 17:58:22 -0800

    Switch example to geojson primitives

--- index.js
+++ index.js
@@ -10,16 +10,25 @@ var point = require('turf-point');
  * @param {String} [units=miles] can be degrees, radians, miles, or kilometers
  * @return {Number} length of the LineString
  * @example
- * var line = turf.linestring([
- *   [-77.031669, 38.878605],
- *   [-77.029609, 38.881946],
- *   [-77.020339, 38.884084],
- *   [-77.025661, 38.885821],
- *   [-77.021884, 38.889563],
- *   [-77.019824, 38.892368]
- * ]);
+ * var line = {
+ *   "type": "Feature",
+ *   "properties": {},
+ *   "geometry": {
+ *     "type": "LineString",
+ *     "coordinates": [
+ *       [-77.031669, 38.878605],
+ *       [-77.029609, 38.881946],
+ *       [-77.020339, 38.884084],
+ *       [-77.025661, 38.885821],
+ *       [-77.021884, 38.889563],
+ *       [-77.019824, 38.892368]
+ *     ]
+ *   }
+ * };
  *
  * var length = turf.lineDistance(line, 'miles');
+ *
+ * //=line
  *
  * //=length
  */
